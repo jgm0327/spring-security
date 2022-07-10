df1174560ecce2809348404667c5246549312163
jgm0327 <gmjang6349@gmail.com>
2022-07-10 12:12:26 +0900

    Create User Entity and UserRepository
    MySQL에 저장할 객체인 User class를 만들고 저장할 수 있도록 JpaRepository를 상속받은 UserRepository 인터페이스를 만듦

---
+++ security/src/main/java/com/jgm/security/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.jgm.security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jgm.security.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+}
